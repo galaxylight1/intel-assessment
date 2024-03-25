8cc72e8dfa11198743ae53efe355db59f24eceac
prateek <prateeksingh8841@gmail.com>
2024-03-25 16:26:40 -0600

    fix: utils

--- app/src/utils/commonKeys.js
+++ app/src/utils/commonKeys.js
@@ -1,2 +1,4 @@
 export const commonKeys = (obj1, obj2) =>
-  Object.keys(obj1).filter((key) => obj2.hasOwnProperty(key));
+  Object.keys(obj1).filter(
+    (key) => obj2.hasOwnProperty(key) && key !== "Datasheet" // 'Datesheet' is added to prevent it getting displayed, because it contains 'View Now' without any hyperlink info
+  );

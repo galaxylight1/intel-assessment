e5e1744f3c748ec5150a08a0359ea0110ffb132d
prateek <prateeksingh8841@gmail.com>
2024-03-30 13:37:06 -0600

    feat: pie chart

--- app/frontend/src/components/Pie Chart/PieChart.jsx
+++ app/frontend/src/components/Pie Chart/PieChart.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, CircularProgress } from "@mui/material";
 import PieChartIcon from "@mui/icons-material/PieChart";
 import { ResponsivePie } from "@nivo/pie";
 import { useTheme } from "@mui/material/styles";
@@ -12,6 +12,7 @@ import { useState, useEffect } from "react";
 
 const PieChart = ({ open, matches }) => {
   const theme = useTheme();
+  const [isLoading, setIsLoading] = useState(true);
   const [pieData, setPieData] = useState([]);
 
   useEffect(() => {
@@ -55,6 +56,7 @@ const PieChart = ({ open, matches }) => {
         ];
 
         setPieData(tempPieData);
+        setIsLoading(false);
       })
       .catch((error) => console.error("Error fetching data: ", error));
   }, []);
@@ -95,101 +97,107 @@ const PieChart = ({ open, matches }) => {
             : { xs: "50px", md: "50px" },
           paddingRight: "1.5rem",
           display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
           transition: theme.transitions.create("padding-left", {
             easing: theme.transitions.easing.easeOut,
             duration: 300,
           }),
         }}
       >
-        <ResponsivePie
-          data={pieData}
-          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-          innerRadius={0}
-          padAngle={0.7}
-          colors={{ scheme: "category10" }}
-          cornerRadius={3}
-          activeOuterRadiusOffset={8}
-          borderWidth={1}
-          valueFormat=" >-.2%"
-          borderColor={{
-            from: "color",
-            modifiers: [["darker", 0.2]],
-          }}
-          arcLinkLabelsSkipAngle={10}
-          arcLinkLabelsTextColor="#333333"
-          arcLinkLabelsThickness={3}
-          arcLinkLabelsColor={{ from: "color" }}
-          arcLabelsSkipAngle={10}
-          arcLabelsTextColor={{
-            from: "color",
-            modifiers: [["darker", 2]],
-          }}
-          defs={[
-            {
-              id: "dots",
-              type: "patternDots",
-              background: "inherit",
-              color: "rgba(255, 255, 255, 0.3)",
-              size: 4,
-              padding: 1,
-              stagger: true,
-            },
-            {
-              id: "lines",
-              type: "patternLines",
-              background: "inherit",
-              color: "rgba(255, 255, 255, 0.3)",
-              rotation: -45,
-              lineWidth: 6,
-              spacing: 10,
-            },
-          ]}
-          fill={[
-            // {
-            //   match: {
-            //     id: "desktop",
-            //   },
-            //   id: "squares",
-            // },
-            // {
-            //   match: {
-            //     id: "mobile",
-            //   },
-            //   id: "dots",
-            // },
-            {
-              match: {
-                id: "server",
+        {isLoading ? (
+          <CircularProgress />
+        ) : (
+          <ResponsivePie
+            data={pieData}
+            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            innerRadius={0}
+            padAngle={0.7}
+            colors={{ scheme: "category10" }}
+            cornerRadius={3}
+            activeOuterRadiusOffset={8}
+            borderWidth={1}
+            valueFormat=" >-.2%"
+            borderColor={{
+              from: "color",
+              modifiers: [["darker", 0.2]],
+            }}
+            arcLinkLabelsSkipAngle={10}
+            arcLinkLabelsTextColor="#333333"
+            arcLinkLabelsThickness={3}
+            arcLinkLabelsColor={{ from: "color" }}
+            arcLabelsSkipAngle={10}
+            arcLabelsTextColor={{
+              from: "color",
+              modifiers: [["darker", 2]],
+            }}
+            defs={[
+              {
+                id: "dots",
+                type: "patternDots",
+                background: "inherit",
+                color: "rgba(255, 255, 255, 0.3)",
+                size: 4,
+                padding: 1,
+                stagger: true,
               },
-              id: "lines",
-            },
-          ]}
-          legends={[
-            {
-              anchor: "bottom",
-              direction: "row",
-              justify: false,
-              translateX: 0,
-              translateY: 56,
-              itemsSpacing: 0,
-              itemWidth: 100,
-              itemHeight: 18,
-              itemTextColor: "#999",
-              itemDirection: "left-to-right",
-              itemOpacity: 1,
-              symbolSize: 18,
-              symbolShape: "circle",
-              effects: [
-                {
-                  on: "hover",
-                  style: {
-                    itemTextColor: "#000",
-                  },
+              {
+                id: "lines",
+                type: "patternLines",
+                background: "inherit",
+                color: "rgba(255, 255, 255, 0.3)",
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10,
+              },
+            ]}
+            fill={[
+              // {
+              //   match: {
+              //     id: "desktop",
+              //   },
+              //   id: "squares",
+              // },
+              // {
+              //   match: {
+              //     id: "mobile",
+              //   },
+              //   id: "dots",
+              // },
+              {
+                match: {
+                  id: "server",
                 },
-              ],
-            },
-          ]}
-        />
+                id: "lines",
+              },
+            ]}
+            legends={[
+              {
+                anchor: "bottom",
+                direction: "row",
+                justify: false,
+                translateX: 30,
+                translateY: 56,
+                itemsSpacing: 0,
+                itemWidth: 100,
+                itemHeight: 18,
+                itemTextColor: "#999",
+                itemDirection: "left-to-right",
+                itemOpacity: 1,
+                symbolSize: 18,
+                symbolShape: "circle",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#000",
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        )}
       </Box>
     </>
   );

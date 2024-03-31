a31e70fee7146a75092f8600319d6aac29fc82d7
prateek <prateeksingh8841@gmail.com>
2024-03-31 17:31:09 -0600

    feat: edit rows and save to localStorage

--- app/frontend/src/components/Table/TableDataGrid.jsx
+++ app/frontend/src/components/Table/TableDataGrid.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridRowModes,
+  GridToolbarContainer,
+  GridActionsCellItem,
+  GridRowEditStopReasons,
+} from "@mui/x-data-grid";
 import { Box, FormControlLabel, LinearProgress, Switch } from "@mui/material";
 import customToolbar from "./CustomToolbar";
 import Snackbar from "@mui/material/Snackbar";
@@ -9,68 +15,10 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { useNavigate } from "react-router-dom";
 import MemoryIcon from "@mui/icons-material/Memory";
 import { useTheme } from "@mui/material/styles";
-
-const columns = [
-  {
-    field: "id",
-    headerName: "ID",
-    minWidth: 100,
-    renderCell: (cellValues) => {
-      return cellValues.row.id + 1;
-    },
-  },
-  {
-    field: "name",
-    headerName: "Product Name",
-    minWidth: 500,
-    renderCell: (cellValues) => {
-      return cellValues.row.name;
-    },
-  },
-  {
-    field: "productCollection",
-    headerName: "Product Collection",
-    minWidth: 300,
-    valueGetter: (val) => {
-      return val;
-    },
-    renderCell: (cellValues) => {
-      return cellValues.row.productCollection;
-    },
-  },
-  {
-    field: "cores",
-    headerName: "# of Cores",
-    minWidth: 100,
-    renderCell: (cellValues) => {
-      return cellValues.row.cores === 0 ? "-" : cellValues.row.cores;
-    },
-  },
-  {
-    field: "processorBaseFreq",
-    headerName: "Processor Base Frequency",
-    minWidth: 150,
-    renderCell: (cellValues) => {
-      return cellValues.row.processorBaseFreq;
-    },
-  },
-  {
-    field: "status",
-    headerName: "Product Status",
-    minWidth: 150,
-    renderCell: (cellValues) => {
-      return cellValues.row.status;
-    },
-  },
-  {
-    field: "segment",
-    headerName: "Vertical Segment",
-    minWidth: 150,
-    renderCell: (cellValues) => {
-      return cellValues.row.segment;
-    },
-  },
-];
+import EditIcon from "@mui/icons-material/Edit";
+// import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Close";
 
 let comparisonProductsArr = [];
 const pageSize = 100;
@@ -113,11 +61,126 @@ export default function Table({
   const [rowSelectionModel, setRowSelectionModel] = useState([]);
   const [isSnackbarVisible, setIsSnackBarVisible] = useState(false);
   const [rows, setRows] = useState([]);
+  const [rowModesModel, setRowModesModel] = useState({});
   const [paginationModel, setPaginationModel] = useState({
     page: pageIdx,
     pageSize: pageSize,
   });
   const [isLoading, setIsLoading] = useState(true);
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      minWidth: 100,
+      renderCell: (cellValues) => {
+        return cellValues.row.id + 1;
+      },
+    },
+    {
+      field: "name",
+      headerName: "Product Name",
+      minWidth: 500,
+      editable: true,
+      renderCell: (cellValues) => {
+        return cellValues.row.name;
+      },
+    },
+    {
+      field: "productCollection",
+      headerName: "Product Collection",
+      minWidth: 300,
+      editable: true,
+      valueGetter: (val) => {
+        return val;
+      },
+      renderCell: (cellValues) => {
+        return cellValues.row.productCollection;
+      },
+    },
+    {
+      field: "cores",
+      headerName: "# of Cores",
+      minWidth: 100,
+      editable: true,
+      renderCell: (cellValues) => {
+        return cellValues.row.cores === 0 ? "-" : cellValues.row.cores;
+      },
+    },
+    {
+      field: "processorBaseFreq",
+      headerName: "Processor Base Frequency",
+      minWidth: 150,
+      editable: true,
+      renderCell: (cellValues) => {
+        return cellValues.row.processorBaseFreq;
+      },
+    },
+    {
+      field: "status",
+      headerName: "Product Status",
+      minWidth: 150,
+      editable: true,
+      renderCell: (cellValues) => {
+        return cellValues.row.status;
+      },
+    },
+    {
+      field: "segment",
+      headerName: "Vertical Segment",
+      minWidth: 150,
+      editable: true,
+      renderCell: (cellValues) => {
+        return cellValues.row.segment;
+      },
+    },
+    {
+      field: "edit",
+      type: "actions",
+      headerName: "Edit Product",
+      width: 100,
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: "primary.main",
+              }}
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          // <GridActionsCellItem
+          //   icon={<DeleteIcon />}
+          //   label="Delete"
+          //   // onClick={handleDeleteClick(id)}
+          //   color="inherit"
+          // />,
+        ];
+      },
+    },
+  ];
 
   // pre-processing, TODO: explore useEffect here
   // useEffect(() => {
@@ -143,7 +206,17 @@ export default function Table({
         return response.json();
       })
       .then((data) => {
-        // data = Object.values(data).slice(0, 100);
+        data = Object.values(data);
+        for (let [key, value] of Object.entries(localStorage)) {
+          const pageNo = key.split(",")[0];
+          const rowId = key.split(",")[1];
+          if (pageNo == pageIdx) {
+            data[rowId] = JSON.parse(value);
+          }
+        }
+
+        console.log("## data -> ", data);
+
         data = data.filter((item) => {
           if (item.name && item.Essentials["Status"]) return true;
         });
@@ -199,6 +272,15 @@ export default function Table({
         return response.json();
       })
       .then((data) => {
+        for (let [key, value] of Object.entries(localStorage)) {
+          const pageNo = key.split(",")[0];
+          const rowId = key.split(",")[1];
+          if (pageNo == pageIdx) {
+            data[rowId] = JSON.parse(value);
+          }
+        }
+
+        console.log("## data -> ", data);
         newJsonData = data;
         const tempJsonData = preProcessing(data, startIdx);
         setIsLoading(false);
@@ -218,6 +300,63 @@ export default function Table({
 
   const handleRowSelectionModel = (newRowSelectionModel) => {
     setRowSelectionModel(newRowSelectionModel);
+  };
+
+  const handleRowModesModelChange = (newRowModesModel) => {
+    // console.log("### handleRowModesModelChange -> ", newRowModesModel);
+    setRowModesModel(newRowModesModel);
+    // setRowModesModel(newRowModesModel);
+  };
+
+  const processRowUpdate = (newRow) => {
+    console.log("### newRow -> ", newRow);
+
+    // update to newJsonData
+    const relativeIdx = newRow.id - startIdx;
+    let obj = newJsonData[relativeIdx];
+    obj.name = newRow.name;
+    obj.Essentials["Product Collection"] = newRow.productCollection;
+    obj.Essentials["Status"] = newRow.status;
+    obj.Essentials["Vertical Segment"] = newRow.segment;
+    obj.Performance["# of Cores"] = `${newRow.cores}`; // make sure # of Cores is string, not integer
+    obj.Performance["Processor Base Frequency"] = newRow.processorBaseFreq;
+
+    localStorage.setItem(`${pageIdx},${relativeIdx}`, JSON.stringify(obj));
+    console.log("### newJsonData", newJsonData);
+
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    return updatedRow;
+    // const updatedRow = { ...newRow, isNew: false };
+    // setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    // return updatedRow;
+  };
+
+  const handleRowEditStop = (params, event) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id) => () => {
+    // console.log("### handleEditClick -> ", id, rowModesModel);
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleCancelClick = (id) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
+    }
   };
 
   // const rows = () => [...newJsonData];
@@ -276,6 +415,11 @@ export default function Table({
         getRowId={(row) => row.id}
         rows={rows}
         columns={columns}
+        editMode="row"
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        onRowEditStop={handleRowEditStop}
+        processRowUpdate={processRowUpdate}
         headerHeight={50}
         rowHeight={50}
         sx={{
